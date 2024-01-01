09ead4febd37a6ca5eec4a9c7447b48b6e371e20
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-01 15:08:09 +0200

    Add validation and input schema to base-entity API

--- server/api/base-entity/index.ts
+++ server/api/base-entity/index.ts
@@ -1,6 +1,8 @@
 import { Category } from '@prisma/client'
+import { baseEntitiesApiInput } from '~/models/shared/schemas'
 import { getBaseEntities } from '~/services/server/base-entity'
+import { validate } from '~/services/shared/validation'
 
 export default defineEventHandler(async ev => {
-  return getBaseEntities(ev.context.prisma, Category.PLANT)
+  return getBaseEntities(ev.context.prisma, ev.context.query.category)
 })
