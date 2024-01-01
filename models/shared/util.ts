1f9848d94121223a93ddb94a01d99cb441f4df74
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-01 16:46:52 +0200

    Refactor utility types for object manipulation

--- models/shared/util.ts
+++ models/shared/util.ts
@@ -8,14 +8,27 @@ export type DeepObject = {
 /**
  * Represents a mutable type derived from a given type `T`.
  * This type recursively removes readonly modifiers from all properties of `T`.
+ * If a property of `T` is an object, this transformation is applied recursively to its properties.
+ * @template T - The type to be transformed into a mutable type.
  */
 export type Mutable<T> = {
   -readonly [P in keyof T]: T[P] extends object ? Mutable<T[P]> : T[P]
 }
 
 /**
+ * Represents a type that converts each property of an object type `T` into an optional property.
+ * If a property of `T` is an object, this transformation is applied recursively to its properties.
+ *
+ * @template T - The type to be transformed into an optional type.
+ */
+export type Optional<T> = {
+  [P in keyof T]?: T[P] extends object ? Optional<T[P]> : T[P]
+}
+
+/**
  * Converts literal types (boolean, number, string) to their corresponding raw types.
  * For other types, it leaves them as is.
+ * @template T - The type to be converted to a raw type.
  */
 export type LiteralToRaw<T> = T extends boolean
   ? boolean
@@ -28,6 +41,7 @@ export type LiteralToRaw<T> = T extends boolean
 /**
  * Converts a literal object type to a raw object type. It applies `LiteralToRaw` to each property of the object,
  * and makes the object mutable using `Mutable`.
+ * @template T - The type to be converted to a raw object type.
  */
 export type LiteralObjectToRaw<T> = Mutable<{
   [P in keyof T]: T[P] extends object
@@ -37,12 +51,14 @@ export type LiteralObjectToRaw<T> = Mutable<{
 
 /**
  * Flattens an object type `T` by extracting the union of types of its properties.
+ * @template T - The type to be flattened.
  */
 export type FlattenObject<T> = T extends object ? T[keyof T] : T
 
 /**
  * Recursively flattens a deep object type `T`.
  * It extracts the union of types of its properties at all depth levels.
+ * @template T - The type to be recursively flattened.
  */
 export type FlattenDeepObject<T> = T extends object
   ? FlattenObject<T> extends infer O
