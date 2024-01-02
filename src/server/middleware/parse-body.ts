6f753523231d6a86824c9d92fcb3d5a72365a99f
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-02 23:34:57 +0200

    Add parse-body and parse-query middleware

--- src/server/middleware/parse-body.ts
+++ src/server/middleware/parse-body.ts
@@ -1,29 +1,29 @@
 import { z } from 'zod'
 import { endpointsInputSchemaMap } from '~/models/shared/schemas'
 import { isKeyOf } from '~/services/shared/util'
-import { validate } from '~/services/shared/validation'
 
 declare module 'h3' {
   interface H3EventContext {
-    query: z.infer<
-      (typeof endpointsInputSchemaMap.query)[keyof typeof endpointsInputSchemaMap.query]
+    body: z.infer<
+      (typeof endpointsInputSchemaMap.body)[keyof typeof endpointsInputSchemaMap.body]
     >
   }
 }
 
 export default defineEventHandler(ev => {
   const { path } = ev.context
-  if (!isKeyOf(endpointsInputSchemaMap.query, path)) {
+  if (!isKeyOf(endpointsInputSchemaMap.body, path)) {
     return
   }
-  const schema = endpointsInputSchemaMap.query[path]
-  const query = getQuery(ev)
-  const parsedQuery = validate(schema, query)
+  const schema = endpointsInputSchemaMap.body[path]
+  const body = readBody(ev)
+  if (!body) return
+  const parsedQuery = schema.safeParse(body)
   if (!parsedQuery.success) {
     throw createError({
       status: 400,
       cause: parsedQuery.error,
     })
   }
-  ev.context.query = parsedQuery.data
+  ev.context.body = parsedQuery.data
 })
