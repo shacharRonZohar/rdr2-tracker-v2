0cb01af77f9b409cc4cf19f413584d55c4df2715
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-06 17:16:35 +0200

    Refactor home page component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,37 +1,10 @@
 <template>
   <!-- <div class="test" @click="console.log">Hi</div> -->
-  <div>
-    <button @click="refreshUserData">Refetch</button>
-    <div v-if="isUserDataPending">Loading...</div>
-    <div v-else-if="userDataError">Error: {{ userDataError }}</div>
-    <div v-else-if="userData">
-      {{ userData }}
-    </div>
-  </div>
+  <div>Home Page</div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   middleware: ['protected-route'],
 })
-
-const { isUserDataPending, refreshUserData, userData, userDataError } =
-  useUserData()
-// const { data, isLoading, error, refetch } = useQuery({
-//   queryKey: ['userData'],
-//   queryFn: async () => {
-//     console.log('fetching')
-//     const res = await $fetch('/api/user-data', {
-//       credentials: 'include',
-//     })
-//     console.log('fetched')
-//     console.log(res)
-//     return res
-//   },
-
-//   staleTime: 1000 * 60 * 60,
-//   refetchOnMount: false,
-//   refetchOnWindowFocus: false,
-//   retry: false,
-// })
 </script>
