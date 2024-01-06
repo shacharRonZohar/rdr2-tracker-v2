3908c3ea828f274b637b74d2f82776a18a611f2c
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-06 17:15:43 +0200

    Add Test link and normalize.scss file

--- src/app.vue
+++ src/app.vue
@@ -4,7 +4,11 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/auth/login">Login</NuxtLink>
     <NuxtLink to="/auth/signup">Signup</NuxtLink>
-
+    <NuxtLink to="/test">Test</NuxtLink>
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+@use '~/assets/styles/normalize.scss';
+</style>
