ea2f454b12a64a10f1df8a6ade591e380c425031
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-06 16:18:16 +0200

    Refactor useUpdateUserData to useMutation

--- src/composables/useUpdateUserData.ts
+++ src/composables/useUpdateUserData.ts
@@ -1,31 +1,13 @@
-import type { UserDataItem } from '~/models/shared/data'
+import type { z } from 'zod'
+import type { dataItemInputSchema } from '~/models/shared/schemas'
 
 export function useUpdateUserData() {
-  // TODO: Split this into a useCustom$Fetch composable
-  const isLoading = ref(false)
-  const error = ref<unknown | null>(null)
-  function updateUserData(userData: UserDataItem) {
-    isLoading.value = true
-    return $fetch('/api/user-data/update', {
-      body: userData,
-      onRequestError: err => {
-        error.value = err
-      },
-      onRequestFinished: () => {
-        isLoading.value = false
-      },
-      onResponseError: err => {
-        error.value = err
-      },
-      onResponseFinished: () => {
-        isLoading.value = false
-      },
-    })
-  }
-
-  return {
-    updateUserDataError: error,
-    updateUserDataIsLoading: isLoading,
-    updateUserData,
-  }
+  return useMutation({
+    mutationKey: ['updateUserData'],
+    mutationFn: (data: z.infer<typeof dataItemInputSchema>) => {
+      return $fetch('/api/user-data/update', {
+        body: data,
+      })
+    },
+  })
 }
