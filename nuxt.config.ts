c3df66aeb81b7e4d8c9ffe84ecb42c27f4ec4126
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2024-01-06 16:32:45 +0200

    Update dependencies and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // devtools: { enabled: true },
   modules: ['@hebilicious/vue-query-nuxt'],
   srcDir: 'src',
-  devtools: { enabled: true },
   runtimeConfig: {
     jwtSecret: process.env.JWT_SECRET,
   },
